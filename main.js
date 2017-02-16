74e4d7b50c69d2824e62bdee3ade703c9879e2a5
kashifali554 <kashifali874@gmail.com>
2017-02-16 14:50:07 -0800

    Lesson 10 complete, card created using javascript and DOM manipulation.

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
-console.log ("Sanity check");
+// console.log ("Sanity check");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+// var cardOne = "queen";
+// var cardTwo = "queen";
+// var cardThree = "king";
+// var cardFour = "king";
 
-if (cardThree === cardFour){
-	alert("You found a match");
-} else {
-	alert("Sorry, Try again");
+// // if (cardThree === cardFour){
+// // 	alert("You found a match");
+// // } else {
+// // 	alert("Sorry, Try again");
+// // }
+
+var cards = document.getElementById("game-board")
+
+function createCards() {
+	for (var i = 0; i < 4; i++){
+	var newDiv = document.createElement("div");
+	newDiv.className = 'card';
+	cards.appendChild(newDiv);
+	}
 }
+createCards();
